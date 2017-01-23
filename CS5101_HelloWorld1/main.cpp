fbb4b766722a26d4533a622439fcff736dccc904
jaredbebb <j.bebb@live.com>
2017-01-22 23:20:19 -0600

    2nd commit

--- CS5101_HelloWorld1/main.cpp
+++ CS5101_HelloWorld1/main.cpp
@@ -1,8 +1,12 @@
 #include <QCoreApplication>
+#include<iostream>
+#include<stdio.h>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    //QCoreApplication a(argc, argv);
 
-    return a.exec();
+
+
+    //return a.exec();
 }
