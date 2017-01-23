597128c9aeccb32c18b425107e4cecadde4a722e
jaredbebb <j.bebb@live.com>
2017-01-22 23:35:03 -0600

    9th commit

--- CS5101_HelloWorld1/main.cpp
+++ CS5101_HelloWorld1/main.cpp
@@ -26,6 +26,6 @@ int main(int argc, char *argv[])
     else{
         std::cout << "Aborted. Incorrect login credentials\n";
     }
-
+    std::cout<<"Program has ended\n";
     //return a.exec();
 }
